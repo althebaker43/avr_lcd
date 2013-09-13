164b4b236dfcc32b69e4128f2aa081ccc7437adc
Allen Baker <althebaker43@gmail.com>
2013-09-13 18:44:20 -0500

    Added begin() statement to properly configure for Newhaven display.

--- arduino_lcd/arduino_lcd.ino
+++ arduino_lcd/arduino_lcd.ino
@@ -8,6 +8,9 @@ const int PIN_D5 = 5;
 const int PIN_D6 = 6;
 const int PIN_D7 = 7;
 
+const int NUM_COLUMNS = 16;
+const int NUM_ROWS = 2;
+
 LiquidCrystal lcd(
     PIN_RS,
     PIN_RW,
@@ -20,6 +23,7 @@ LiquidCrystal lcd(
 
 void setup() {
   
+  lcd.begin(NUM_COLUMNS, NUM_ROWS);
   lcd.print("Hello, World!");
 }
 
